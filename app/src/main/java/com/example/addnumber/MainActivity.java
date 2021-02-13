983ffaf2ad04a2a2e0c8509f70347a8d968fc351
JANINDU MALEESHA <janindumaleesha99@gmail.com>
2021-02-13 15:03:36 +0530

    first commit

---
+++ app/src/main/java/com/example/addnumber/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.addnumber;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText num1;
+    private EditText num2;
+    private Button add;
+    private TextView result;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        num1 = (EditText)findViewById(R.id.etNum1);
+        num2 = (EditText)findViewById(R.id.etNum2);
+        add = (Button)findViewById(R.id.btnAdd);
+        result = (TextView)findViewById(R.id.background);
+
+        add.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View view){
+                int number1 = Integer.parseInt(num1.getText().toString());
+                int number2 = Integer.parseInt(num2.getText().toString());
+                int sum = number1 + number2;
+                result.setText("Answer: " + String.valueOf(sum));
+            }
+        });
+    }
+}
