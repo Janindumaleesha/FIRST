b6e737d3612a4a70b40cfd4e683ec75e27d54b55
SiriwardanaT <74727810+SiriwardanaT@users.noreply.github.com>
2021-02-13 23:39:18 +0530

    problem slove!

--- app/src/main/java/com/example/addnumber/MainActivity.java
+++ app/src/main/java/com/example/addnumber/MainActivity.java
@@ -10,29 +10,30 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText num1;
-    private EditText num2;
-    private Button add;
-    private TextView result;
+    EditText numOne;
+    EditText numTwo;
+    Button add;
+    TextView reuslts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        num1 = (EditText)findViewById(R.id.etNum1);
-        num2 = (EditText)findViewById(R.id.etNum2);
-        add = (Button)findViewById(R.id.btnAdd);
-        result = (TextView)findViewById(R.id.background);
+        numOne = findViewById(R.id.num1);
+        numTwo = findViewById(R.id.num2);
+        add = findViewById(R.id.button);
+        reuslts = findViewById(R.id.textView);
 
-        add.setOnClickListener(new View.OnClickListener(){
-
-            public void onClick(View view){
-                int number1 = Integer.parseInt(num1.getText().toString());
-                int number2 = Integer.parseInt(num2.getText().toString());
-                int sum = number1 + number2;
-                result.setText("Answer: " + String.valueOf(sum));
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               int sum = Integer.parseInt(numOne.getText().toString()) +  Integer.parseInt(numTwo.getText().toString());
+                reuslts.setText("results is "+sum);
             }
         });
+
+
+
     }
 }
