d6cae4c52498843c4b30d5be407ff268430f46a8
JANINDU MALEESHA <janindumaleesha99@gmail.com>
2021-02-15 20:29:43 +0530

    i impliment sub, div, multi

--- app/src/main/java/com/example/addnumber/MainActivity.java
+++ app/src/main/java/com/example/addnumber/MainActivity.java
@@ -13,6 +13,9 @@ public class MainActivity extends AppCompatActivity {
     EditText numOne;
     EditText numTwo;
     Button add;
+    Button sub;
+    Button div;
+    Button multi;
     TextView reuslts;
 
     @Override
@@ -23,6 +26,9 @@ public class MainActivity extends AppCompatActivity {
         numOne = findViewById(R.id.num1);
         numTwo = findViewById(R.id.num2);
         add = findViewById(R.id.button);
+        sub = findViewById(R.id.button2);
+        div = findViewById(R.id.button3);
+        multi = findViewById(R.id.button4);
         reuslts = findViewById(R.id.textView);
 
         add.setOnClickListener(new View.OnClickListener() {
@@ -30,6 +36,26 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                int sum = Integer.parseInt(numOne.getText().toString()) +  Integer.parseInt(numTwo.getText().toString());
                 reuslts.setText("results is "+sum);
+            }
+        });
+        sub.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                int su = Integer.parseInt(numOne.getText().toString()) -  Integer.parseInt(numTwo.getText().toString());
+                reuslts.setText("results is "+su);
+            }
+        });
+        div.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int di = Integer.parseInt(numOne.getText().toString()) / Integer.parseInt(numTwo.getText().toString());
+                reuslts.setText("results is "+di);
+            }
+        });
+        multi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int mu = Integer.parseInt(numOne.getText().toString()) * Integer.parseInt(numTwo.getText().toString());
+                reuslts.setText("results is "+mu);
             }
         });
 
